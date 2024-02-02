ebb079bbd9a3563c1253a8798e92dfb3030b1151
Lukas Chenghan Huan <lukas@huan.eu>
2024-02-02 15:41:33 +0100

    add debugging info

--- bert/src/dna_data.py
+++ bert/src/dna_data.py
@@ -16,38 +16,13 @@ def build_dna_dataloader(
             'concatenate, use the --concat_tokens ' +
             'argument when creating your MDS dataset with convert_dataset.py')
 
-    # build streams
-    streams_dict = cfg.dataset.get('streams', None)
-    streams = None
-    if streams_dict is not None:
-        streams = []
-        for _, stream in streams_dict.items():
-            streams.append(
-                Stream(
-                    remote=stream.get('remote', None) or
-                    cfg.dataset.get('remote', None),
-                    local=stream.get('local', None) or
-                    cfg.dataset.get('local', None),
-                    split=stream.get('split', None) or
-                    cfg.dataset.get('split', None),
-                    proportion=stream.get('proportion', None),
-                    repeat=stream.get('repeat', None),
-                    samples=stream.get('samples', None),
-                    download_retry=stream.get('download_retry', None) or
-                    cfg.dataset.get('download_retry', 2),
-                    download_timeout=stream.get('download_timeout', None) or
-                    cfg.dataset.get('download_timeout', 60),
-                    validate_hash=stream.get('validate_hash', None) or
-                    cfg.dataset.get('validate_hash', None),
-                    keep_zip=stream.get('keep_zip', None) or
-                    cfg.dataset.get('keep_zip', False),
-                    keep_raw=stream.get('keep_raw', None) or
-                    cfg.dataset.get('keep_raw', True),
-                ))
-
     assert cfg.dataset.local is not None, "No local dataset provided"
     dataset = load_dataset(cfg.dataset.local)
     dataset = dataset[cfg.dataset.split]
+
+    print("===ds===")
+    print(dataset)
+    print(device_batch_size)
 
     return DataLoader(
         dataset,
