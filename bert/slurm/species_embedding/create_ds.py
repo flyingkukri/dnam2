dd1c65e6570a584d484953279025c58f262df854
Lukas Chenghan Huan <lukas@huan.eu>
2024-07-18 11:49:18 +0200

    refactored create_ds

--- bert/slurm/species_embedding/create_ds.py
+++ bert/slurm/species_embedding/create_ds.py
@@ -5,35 +5,41 @@ from transformers import AutoTokenizer
 import pandas as pd
 
 dataset_path = "/s/project/semi_supervised_multispecies/all_fungi_reference/fungi/Annotation/Sequences/AAA_Concatenated/pretokenized/speciesdownstream300"
-#dataset_path = "../../batch"
+dataset_path = "../../batch"
 print('started loading dataset')
 dataset = load_from_disk(dataset_path)
+
+dataset = dataset["train"]
 
 #dataset = dataset[:10]
 
 print('loaded dataset')
 
-def delete_2nd_fun (example):
-    del example["input_ids"][1]
-    return example
+def create_species_embed_ds(dataset):
+    def delete_2nd_fun (example):
+        del example["input_ids"][1]
+        return example
 
-print('deleting the second (species) token')
-updated_dataset = dataset.map(delete_2nd_fun)
-print('deleted the second token')
+    print('deleting the second (species) token')
+    updated_dataset = dataset.map(delete_2nd_fun)
+    print('deleted the second token')
 
 
-tokenizer = AutoTokenizer.from_pretrained("gagneurlab/SpeciesLM", revision="downstream_species_lm")
-start_species_ids = tokenizer.convert_tokens_to_ids(["GGGGGG"])[0]
-species_col = dataset["species_name"]
-print('extracted species column')
-species_ids = tokenizer.convert_tokens_to_ids(species_col)
-print('converted species tokens to ids')
-species_ids = [species_id - start_species_ids for species_id in species_ids]
-print('subtracted_starting species id')
-# delete the species token in the input_ids
+    tokenizer = AutoTokenizer.from_pretrained("gagneurlab/SpeciesLM", revision="downstream_species_lm")
+    start_species_ids = tokenizer.convert_tokens_to_ids(["GGGGGG"])[0]
+    species_col = dataset["species_name"]
+    print('extracted species column')
+    species_ids = tokenizer.convert_tokens_to_ids(species_col)
+    print('converted species tokens to ids')
+    species_ids = [species_id - start_species_ids for species_id in species_ids]
+    print('subtracted_starting species id')
+    # delete the species token in the input_ids
 
-updated_dataset = updated_dataset.add_column("species_ids", species_ids)
-print('added new species embedding column to the dataset')
+    updated_dataset = updated_dataset.add_column("species_id", species_ids)
+    print('added new species embedding column to the dataset')
+    return updated_dataset
 
-updated_dataset.save_to_disk("../../full_embed")
+updated_dataset = create_species_embed_ds(dataset)
+
+updated_dataset.save_to_disk("../../batch_embed")
 print("saved to disk")
